28c2eefc287ac8e9a0c038a52a5ed65bc3ac9e9f
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2024-09-16 00:10:28 +0530

    Completed

--- Project 1 - XO Game/src/App.jsx
+++ Project 1 - XO Game/src/App.jsx
@@ -3,45 +3,118 @@ import GameBoard from "./components/GameBoard";
 import { useState } from "react";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combination";
+import GameOver from "./components/GameOver";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function derivedActivePlayer(gameTurns) {
-  let currPlayer = 'X';
+  let currPlayer = "X";
 
-  if (gameTurns.length > 0 && gameTurns[0].player === 'X'){
-    currPlayer = 'O';
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currPlayer = "O";
   }
 
-  return currPlayer
+  return currPlayer;
 }
 
+function deriveWinner(gameBoard, players) {
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquaresSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquaresSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquaresSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquaresSymbol &&
+      firstSquaresSymbol == secondSquaresSymbol &&
+      firstSquaresSymbol == thirdSquaresSymbol
+    ) {
+      winner = players[firstSquaresSymbol];
+    }
+  }
+  return winner;
+}
+
+function deriverGameBoard(gameTurns) {
+  let gameBoard = [...initialGameBoard.map((array) => [...array])];
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  return gameBoard;
+}
 function App() {
-  const [ gameTurns, setGameTurns ] = useState([])
-  // const [activePlayer, setActivePlayer] = useState('X')
+  const [players, setPlayers] = useState({
+    X: "Player 1",
+    O: "Player 2",
+  });
+  const [gameTurns, setGameTurns] = useState([]);
+
   const activePlayer = derivedActivePlayer(gameTurns);
 
+  const gameBoard = deriverGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, players);
+
+  const hasDraw = gameTurns.length == 9 && !winner;
+
   function handleSelectSquare(rowIdx, colIdx) {
-    // setActivePlayer((curActiveLayer) => curActiveLayer === 'X' ? 'O' : 'X')
     setGameTurns((prevTurns) => {
       const currPlayer = derivedActivePlayer(prevTurns);
       const updatedTurns = [
-        { square: { row: rowIdx, col: colIdx } ,player : currPlayer},
+        { square: { row: rowIdx, col: colIdx }, player: currPlayer },
         ...prevTurns,
       ];
 
-      return updatedTurns
-    })
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newChange) {
+    setPlayers((prevNames) => {
+      return {
+        ...prevNames,
+        [symbol]: newChange,
+      };
+    });
   }
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="Player 1" symbol="X" isActive={activePlayer === "X"} />
-          <Player name="Player 2" symbol="O" isActive={activePlayer === "O"} />
+          <Player
+            name="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            name="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          turns={gameTurns}
-        />
+        {(winner || hasDraw) && (
+          <GameOver winner={winner} onRestart={handleRestart} />
+        )}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}></Log>
     </main>
--- Project 1 - XO Game/src/components/GameBoard.jsx
+++ Project 1 - XO Game/src/components/GameBoard.jsx
@@ -1,34 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-	let gameBoard = initialGameBoard;
-
-	for(const turn of turns) {
-		const { square, player } = turn;
-		const {row, col} = square;
-
-		gameBoard[row][col] = player;
-	}
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(eachRowIdx, colIdx) {
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedGameBoard = [
-  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //     ];
-  //     updatedGameBoard[eachRowIdx][colIdx] = activePlayerSymbol;
-  //     return updatedGameBoard;
-  //   });
-
-	// 	onSelectSquare();
-  // }
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((eachRow, eachRowIdx) => (
+      {board.map((eachRow, eachRowIdx) => (
         <li key={eachRowIdx}>
           <ol>
             {eachRow.map((playerSymbol, colIdx) => (
