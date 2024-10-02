8ace994550a8361f05a19604faa4ec16d19fcf9f
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2024-10-02 13:17:21 +0530

    completed

--- PROJECT 2 - Investment Calculator/src/components/Results.jsx
+++ PROJECT 2 - Investment Calculator/src/components/Results.jsx
@@ -1,8 +1,43 @@
-import { calculateInvestmentResults } from "../util/investment.js"
+import { calculateInvestmentResults, formatter } from "../util/investment.js";
 
 export default function Results({ userInput }) {
   let resultData = calculateInvestmentResults(userInput);
+  let inititalInvestment =
+    resultData[0].valueEndOfYear -
+    resultData[0].interest -
+    resultData[0].annualInvestment;
 
-  console.log(resultData);
-  return <p>Results</p>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultData.map((yearData) => {
+          let totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            inititalInvestment;
+
+          let totalAmountInterested =
+            yearData.valueEndOfYear - yearData.interest;
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInterested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
